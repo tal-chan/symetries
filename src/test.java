fb9da628f22fef932642a5592e4801b9b18289eb
Klomak <klomak@gmail.com>
2013-11-30 14:04:26 +0100

    BufferedImage pour obtenir les pixels d'une image

--- src/test.java
+++ src/test.java
@@ -1,8 +1,33 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 
 public class test {
 	
-	// commentaire de Chloé 
-	
+
 	int truc;
 	test(){truc=0;}
+	
+	
+	public static void main(String[] args){
+
+		BufferedImage imgBuffer;
+		try {
+			imgBuffer = ImageIO.read(new File("carre.png"));
+			byte[] pixels = (byte[])imgBuffer.getRaster().getDataElements(0, 0, imgBuffer.getWidth(), imgBuffer.getHeight(), null);
+			for(int i=0; i< pixels.length;i++){
+						System.out.println(pixels[i]);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
 }
