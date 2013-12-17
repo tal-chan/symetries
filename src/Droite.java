d7ca34592c39b6538190d0a1c567a7b185e7b426
Chantal Ding <chantal.j.ding@gmail.com>
2013-12-17 22:18:00 +0100

    changement d'orientation pour theta, calcul du determinant jacobien, debut du paragraphe sampling

--- src/Droite.java
+++ src/Droite.java
@@ -21,7 +21,7 @@ public class Droite {
 	public Droite(Point p1, Point p2){
 		double d = p1.distanceFrom(p2);		
 		this.cosTheta = ((double)p2.getX() - p1.getX())/d;
-		this.sinTheta = ((double)p1.getY() - p2.getY())/d; // because of the tricky orientation 
+		this.sinTheta = ((double)p2.getY() - p1.getY())/d;
 		
 		//on veut avoir Theta entre -Pi/2 et Pi/2 donc :
 		if(this.cosTheta<0){
@@ -29,12 +29,12 @@ public class Droite {
 			this.sinTheta = - this.sinTheta;
 		}
 		
-		//System.out.println(cosTheta*cosTheta+sinTheta*sinTheta);
+		System.out.println(cosTheta*cosTheta+sinTheta*sinTheta);
 		
 		// droite d'equation ax + by + c = 0 (a = cosTheta, b = sinTheta, c = r cherché)
 		double xMilieu = (((double)p2.getX()) + p1.getX())/2;
 		double yMilieu = (((double)p2.getY()) + p1.getY())/2;
-		this.r =  this.cosTheta * xMilieu - this.sinTheta * yMilieu;	
+		this.r =  this.cosTheta * xMilieu + this.sinTheta * yMilieu;	
 	}
 	
 	@Override
@@ -56,9 +56,9 @@ public class Droite {
 	
 	public int[] toCoordinates () {
 		int x = (int) (r*cosTheta);
-		int y =  -(int) (r*sinTheta);
+		int y =  (int) (r*sinTheta);
 		if(sinTheta!=0.){
-			int[]t={x,y,x+400,(int)(y+cosTheta/sinTheta*400)};;
+			int[]t={x,y,x+400,(int)(y-cosTheta/sinTheta*400)};;
 			return t;
 			
 		}
